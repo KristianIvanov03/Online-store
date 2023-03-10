15f865fa888c151a6b2cf3baa2926b5e2de7b2fe
Your Name <you@example.com>
2023-03-10 20:07:00 +0200

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
+import { Routes, Route} from 'react-router-dom'
 import './App.css';
+import Cart from './components/Cart';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <NavBar/>
+    <Routes>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </>
   );
 }
 
