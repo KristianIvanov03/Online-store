4e78b8d1a2385bba0a47b32e0e075015006c2954
Your Name <you@example.com>
2023-03-11 16:59:32 +0200

    added product data file and made some changes in index.js file

---
+++ src/backend/products.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "6.1-inch display",
+        price: 999,
+        image: "https://res.cloudinary.com/drsgsaay7/image/upload/v1678546624/onlineShop/iphone12pro_dwprcx.jpg"
+    },
+    {
+        id: 2,
+        name: "iPhone 12",
+        desc: "5.1-inch display",
+        price: 699,
+        image: "https://res.cloudinary.com/drsgsaay7/image/upload/v1678546621/onlineShop/iphone12_fhdjzo.jpg"
+    },
+    {
+        id: 3,
+        name: "Galaxy S",
+        desc: "6.5-inch display",
+        price: 399,
+        image: "https://res.cloudinary.com/drsgsaay7/image/upload/v1678546616/onlineShop/galaxyS_q9n4yx.png"
+    }
+];
+module.exports = products;
