bc5964906403c3e1a5c3db8ac2ff39f5c899e735
Your Name <you@example.com>
2023-03-12 12:17:31 +0200

    added product reducer to store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import App from './App';
 import { BrowserRouter} from 'react-router-dom';
 import { configureStore} from '@reduxjs/toolkit';
 import { Provider} from 'react-redux';
+import productsReducer from './features/productsSlice';
 
 const store = configureStore({
-  reducer:{}
+  reducer:{
+    products: productsReducer
+  }
 })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
