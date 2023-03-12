ad63fd912bef9eea716f309d824e454e6df387dd
Your Name <you@example.com>
2023-03-12 12:14:49 +0200

    added redux store and slice

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter} from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom';
+import { configureStore} from '@reduxjs/toolkit';
+import { Provider} from 'react-redux';
 
+const store = configureStore({
+  reducer:{}
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
