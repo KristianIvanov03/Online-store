55d791d410a21abc4c9157a213e6ef1bb45da79e
Your Name <you@example.com>
2023-03-15 14:41:21 +0200

    added decreaseCart action creator

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -37,6 +37,28 @@ const cartSlice = createSlice({
             toast.error(`${action.payload.name} removed from cart `, {
                 position: "bottom-left"
             });
+        },
+        decreaseCart(state, action){
+            const itemIndex = state.cartItems.findIndex(
+                cartItem => cartItem.id === action.payload.id
+            )
+
+            if (state.cartItems[itemIndex].cartQuantity > 1){
+                state.cartItems[itemIndex].cartQuantity -= 1;
+                toast.info(`Decreased ${action.payload.name} cart quantity`, {
+                    position: "bottom-left"
+                })
+            }else if(state.cartItems[itemIndex].cartQuantity === 1){
+                const nextCartItems = state.cartItems.filter(
+                    (cartItem) => cartItem.id !== action.payload.id
+                );
+
+                state.cartItems = nextCartItems;
+                toast.error(`${action.payload.name} removed from cart`, {
+                    position: 'bottom-left'
+                })
+            }
+            localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
         }
     }
 });
